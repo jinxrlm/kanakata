77f727d0c8c36371d2402503262a7c7f776406aa
jinxrlm <janplexicon@gmail.com>
2022-11-10 09:46:59 +0100

    day 4

---
+++ jpscript.js
@@ -0,0 +1,21 @@
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    .forEach(tooltip => {
+      new bootstrap.Tooltip(tooltip)
+    })
+
+window.onload = function() {
+    var cur_card = -1;
+    cards = [
+        'card1',
+        'card2',
+        'card3',
+        'card4'
+    ];
+
+var myInterval = setInterval(function() {
+    if (cur_card >= 0) document.getElementById(cards[cur_card % cards.length]).classList.toggle("jp-card-sel");
+    document.getElementById(cards[(++cur_card) % cards.length]).classList.toggle("jp-card-sel");
+    console.log(cur_card);
+}, 3000);
+}
