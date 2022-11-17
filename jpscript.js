70565babd4216010c80691b43e49c7d043ba9f75
jinxrlm <janplexicon@gmail.com>
2022-11-17 16:03:10 +0100

    Day idk 8?
    Co-authored-by: Hemnghaderi <Hemnghaderi@users.noreply.github.com>

--- jpscript.js
+++ jpscript.js
@@ -6,16 +6,10 @@ document.querySelectorAll('[data-bs-toggle="tooltip"]')
 
 window.onload = function() {
     var cur_card = -1;
-    cards = [
-        'card1',
-        'card2',
-        'card3',
-        'card4'
-    ];
+    cards = ['card1','card2','card3','card4'];
 
 var myInterval = setInterval(function() {
     if (cur_card >= 0) document.getElementById(cards[cur_card % cards.length]).classList.toggle("jp-card-sel");
     document.getElementById(cards[(++cur_card) % cards.length]).classList.toggle("jp-card-sel");
-    console.log(cur_card);
 }, 3000);
 }
